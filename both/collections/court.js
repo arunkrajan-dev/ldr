d52e58180ec89f62721a7ea26f353f3bb164ff16
arunsugan <arunmail2u@gmail.com>
2016-01-29 14:26:01 +0000

    formated panels

--- both/collections/court.js
+++ both/collections/court.js
@@ -1,8 +1,8 @@
 this.Courts = new Mongo.Collection("Court");
-this.Events = new Meteor.Collection('events');
+//this.Events = new Meteor.Collection('events');
 // Court.insert({name:'Chennai High Court'});
 // Court.insert({name:'Madurai High Court'});
 // Court.insert({name:'Chennai civil Court'});
 // Court.insert({name:'Chennai Family Court'});
 
-console.log("Court db updated:" + JSON.stringify(Courts.find().fetch(), null, 4));
+//console.log("Court db updated:" + JSON.stringify(Courts.find().fetch(), null, 4));
