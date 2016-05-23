ad9e51cb8d88d9a9c5a6ba4ac6701804e55ec0f6
arunsugan <arunmail2u@gmail.com>
2016-05-23 11:38:44 +0000

    updated for limitation edit

--- client/views/caseprofile/edit/edit.js
+++ client/views/caseprofile/edit/edit.js
@@ -17,12 +17,13 @@ Template.CaseprofileEditEditForm.rendered = function() {
 
 	pageSession.set("caseprofileEditEditFormInfoMessage", "");
 	pageSession.set("caseprofileEditEditFormErrorMessage", "");
-
-	$(".input-group.date").each(function() {
-		$(this).find("input[type='text']").datetimepicker({
-			format: 'DD/MM/YYYY',
-			});
-	});
+	
+	initDateTimePickers();
+	// $(".input-group.date").each(function() {
+	// 	$(this).find("input[type='text']").datetimepicker({
+	// 		format: 'DD/MM/YYYY',
+	// 		});
+	// });
 
 	$("input[type='file']").fileinput();
 	$("select[data-role='tagsinput']").tagsinput();
@@ -95,9 +96,52 @@ Template.CaseprofileEditEditForm.events({
 		e.preventDefault();
 
 		/*BACK_REDIRECT*/
-	}
+	},
+	"change #limitation": function(e, t) {
+		
+		$('#limitationDate').val(moment($('#judgementDate').val(), "DD/MM/YYYY").add($('#limitation').val(), 'days').format("(ddd) DD/MM/YYYY"));
+		var date1 = new Date();
+		var date2 = new Date(moment($('#limitationDate').val(), "(ddd) DD/MM/YYYY"));
+		var timeDiff = date2.getTime() - date1.getTime();
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+		var txt = "";
+		if(diffDays == 0) {
+			txt = "Today";
+		} else if(diffDays == 1) {
+			txt = "tomorrow";
+		} else if(diffDays == -1) {
+			txt = "YesterDay";
+		} else if(diffDays < 0) {
+			txt = (diffDays * -1) + " Days Exceeded";
+		} else {
+			txt = diffDays + " Days More";
+		}
+		$('#limitationDiff').text(txt);
+		debugger;
 
-	
+		//$('#limitationDiff').text(moment($('#limitationDate').val(), "DD/MM/YYYY").fromNow());
+	},
+	"keyup #limitation": function(e, t) {
+		debugger;
+		$('#limitationDate').val(moment($('#judgementDate').val(), "DD/MM/YYYY").add($('#limitation').val(), 'days').format("(ddd) DD/MM/YYYY"));
+		var date1 = new Date();
+		var date2 = new Date(moment($('#limitationDate').val(), "(ddd) DD/MM/YYYY"));
+		var timeDiff = date2.getTime() - date1.getTime();
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+		var txt = "";
+		if(diffDays == 0) {
+			txt = "Today";
+		} else if(diffDays == 1) {
+			txt = "tomorrow";
+		} else if(diffDays == -1) {
+			txt = "YesterDay";
+		} else if(diffDays < 0) {
+			txt = (diffDays * -1) + " Days Exceeded";
+		} else {
+			txt = diffDays + " Days More";
+		}
+		$('#limitationDiff').text(txt);
+	}	
 });
 
 Template.CaseprofileEditEditForm.helpers({
@@ -118,3 +162,40 @@ Template.CaseprofileEditEditForm.helpers({
 	}
 	
 });
+
+var initDateTimePickers = function() {
+	$(".input-group.date").each(function() {
+		$(this).find("input[type='text']").datetimepicker({
+			format: 'DD/MM/YYYY',
+			});
+	});
+	
+	//this.$('#picker-1, #picker-2').datetimepicker();
+
+	$('#judgementDate').on("dp.change", function(e) {
+		$('#limitationDate').val(moment(e.date).add($('#limitation').val(), 'days').format("(ddd) DD/MM/YYYY"));
+		var date1 = new Date();
+		var date2 = new Date(moment($('#limitationDate').val(), "(ddd) DD/MM/YYYY"));
+		var timeDiff = date2.getTime() - date1.getTime();
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+		var txt = "";
+		if(diffDays == 0) {
+			txt = "Today";
+		} else if(diffDays == 1) {
+			txt = "tomorrow";
+		} else if(diffDays == -1) {
+			txt = "YesterDay";
+		} else if(diffDays < 0) {
+			txt = (diffDays * -1) + " Days Exceeded";
+		} else {
+			txt = diffDays + " Days More";
+		}
+		$('#limitationDiff').text(txt);
+	}); 
+	
+    $("#dob").on("dp.change", function (e) {
+        //console.log(e.date);
+        var years = moment().diff(e.date, 'years');
+        $('#age').val(years);
+    });
+};
